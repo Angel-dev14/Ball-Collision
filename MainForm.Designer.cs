2dc165bfb6b8007c25028aec041b4743207e3fe4
Petrusevski <angelpetrusevski@gmail.com>
2021-05-31 20:41:10 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace BallCollision
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Generator = new System.Windows.Forms.Timer(this.components);
+            this.BallShrink = new System.Windows.Forms.Timer(this.components);
+            this.status = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblPoints = new System.Windows.Forms.ToolStripStatusLabel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Generator
+            // 
+            this.Generator.Tick += new System.EventHandler(this.Generator_Tick);
+            // 
+            // BallShrink
+            // 
+            this.BallShrink.Tick += new System.EventHandler(this.BallShrink_Tick);
+            // 
+            // status
+            // 
+            this.status.Tick += new System.EventHandler(this.checkStatus);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.MediumPurple;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.lblPoints});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 544);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1043, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(46, 17);
+            this.toolStripStatusLabel1.Text = "Points: ";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(13, 17);
+            this.lblPoints.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(1043, 566);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainForm";
+            this.Text = "Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainForm_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer Generator;
+        private System.Windows.Forms.Timer BallShrink;
+        private System.Windows.Forms.Timer status;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblPoints;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallCollision
+{
+    public partial class MainForm : Form
+    {
+
+        public Scene Scene { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Start();
+
+        }
+        private void Start()
+        {
+            Scene = new Scene(this.Width, this.Height);
+            GeneratePlayerScreen();
+
+            Generator.Interval = 5000;
+            Generator.Start();
+
+            BallShrink.Interval = 3000;
+            BallShrink.Start();
+
+            status.Interval = 1000;
+            status.Start();
+            DoubleBuffered = true;
+        }
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Scene.DrawAll(e.Graphics);
+            lblPoints.Text = Scene.Points.ToString();
+           
+        }
+        private void GeneratePlayerScreen()
+        {
+            Scene.GenerateBalls();
+            Invalidate();
+        }
+
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            Scene.AddPlayerBall(e.Location);
+            Invalidate();
+        }
+
+        private void MainForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Scene.PlayerBall == null)
+                return;
+            char pressed_key = Char.ToLower(e.KeyChar);
+
+            switch(pressed_key)
+            {
+                case 'w':
+                    Scene.MoveUp();
+                    break;
+                case 's':
+                    Scene.MoveDown();
+                    break;
+                case 'a':
+                    Scene.MoveLeft();
+                    break;
+                case 'd':
+                    Scene.MoveRight();
+                    break;
+                default: break;
+            }
+            Invalidate();
+        }
+
+        private void Generator_Tick(object sender, EventArgs e)
+        {
+            Scene.GenerateBall();
+            Invalidate();
+        }
+
+        private void BallShrink_Tick(object sender, EventArgs e)
+        {
+            Scene.Shrink();
+            Invalidate();
+        }
+
+        private void checkStatus(object sender, EventArgs e)
+        {
+            if (Scene.GameOver)
+            {
+                Scene.GameOver = false;
+                status.Stop();
+                var result = MessageBox.Show("Играта заврши, изгубивте","Game Over",MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel)
+                {
+                    this.Close();
+                }
+                else {
+                    NewGame();
+                }
+            }
+        }
+        private void Reset()
+        {
+            lblPoints.Text = "0";
+            Scene = new Scene(this.Width, this.Height);
+            Generator.Stop();
+            BallShrink.Stop();
+        }
+        private void NewGame()
+        {
+            Reset();
+            Start();
+            Invalidate();
+        }
+    }
+}
